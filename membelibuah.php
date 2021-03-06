f9934a065d78d6e01781cf140079d9182a85bb26
ahmadekahanggani2710 <79893062+ahmadekahanggani2710@users.noreply.github.com>
2021-03-06 14:35:38 +0700

    Add files via upload

---
+++ membelibuah.php
@@ -0,0 +1,10 @@
+<?php
+
+$namadepan = "ahmad eka";
+$namabelakang = "Hanggani";
+$jumlahbarang = "3";
+$harga = "Rp.100,000";
+
+echo "nama saya adalah $namadepan $namabelakang saya membeli buah naga sebanyak $jumlahbarang seharga $harga";
+
+?>
